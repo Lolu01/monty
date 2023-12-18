9b0251e4e79de927a8a8efcc13ffd6393f0cf716
Lolu01 <jaiyeolalulu@gmail.com>
2023-12-18 11:44:36 -0800

    Refix

--- pchar.c
+++ pchar.c
@@ -1,13 +1,12 @@
 #include "monty.h"
 
 /**
- * f_pchar - prints the char at the top of the stack.
+ * f_pchar - prints the char at the top of the stack,
  * followed by a new line
  * @head: stack head
  * @counter: line_number
  * Return: no return
- */
-
+*/
 void f_pchar(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
@@ -15,11 +14,11 @@ void f_pchar(stack_t **head, unsigned int counter)
 	h = *head;
 	if (!h)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack too empty\n", counter);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*head);
-                exit(EXIT_FAILURE);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
 	if (h->n > 127 || h->n < 0)
 	{
