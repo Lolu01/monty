3ce0eaf72c966fa2367e815678710efcf00429f1
Lolu01 <jaiyeolalulu@gmail.com>
2023-12-18 11:24:24 -0800

    resolve

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdio.h>
 #include "monty.h"
 
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
 * Return: 0 on success
 */
-
-bus_t bus = {NULL, NULL, NULL, 0};
-
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -18,17 +19,16 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
-	if (argc  != 2)
+	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE : monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	bus.file = file;
-
 	if (!file)
 	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (read_line > 0)
@@ -37,7 +37,6 @@ int main(int argc, char *argv[])
 		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-
 		if (read_line > 0)
 		{
 			execute(content, &stack, counter, file);
@@ -46,6 +45,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-
 return (0);
 }
